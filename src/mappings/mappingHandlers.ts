e3414ee6dc14dd064ac113955fde398e8552c052
Hayden Fan <haosong0323@gmail.com>
2021-12-28 18:11:17 +0800

    test: wip

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,14 +1,23 @@
 import { MoonbeamCall, MoonbeamEvent } from '@subql/contract-processors/dist/moonbeam'
-import { Approval } from '../types'
+import { Approval, Transaction } from '../types'
 import { ApproveCallArgs, createTransfer, TransferEventArgs } from '../utils/createTransferRecord'
 
 export async function handleMoonriverEvent(event: MoonbeamEvent<TransferEventArgs>): Promise<void> {
-  const from = event.args.from
-  const to = event.args.to
-  const amount = event.args.value.toBigInt()
-  const tokenAddress = event.address
+  const transaction = new Transaction(event.transactionHash);
 
-  logger.info(tokenAddress)
+    transaction.value = event.args.value.toBigInt();
+    transaction.from = event.args.from;
+    transaction.to = event.args.to;
+    transaction.contractAddress = event.address;
+
+    await transaction.save();
+    
+  // const from = event.args.from
+  // const to = event.args.to
+  // const amount = event.args.value.toBigInt()
+  // const tokenAddress = event.address
+
+  logger.info(event.address)
 
   // await createTransfer(from, to, tokenAddress, amount, event)
 }
